34eb4e22bdb6b96087739ef0194d611063506dfe
MrFlufff <156640574+MrFlufff@users.noreply.github.com>
2025-08-08 19:19:59 +0800

    Add files via upload

---
+++ Home/script.js
@@ -0,0 +1,8 @@
+// 禁止右键菜单
+document.oncontextmenu = function () { return false; };
+// 禁止文字选择
+document.onselectstart = function () { return false; };
+// 禁止复制
+document.oncopy = function () { return false; };
+// 禁止剪切
+document.oncut = function () { return false; };
